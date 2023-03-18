7df4a91cef1b312874763c0ca4aaaa46146fee25
taek0622 <min4644@gmail.com>
2023-03-19 05:09:19 +0900

    Feat:sparkles:: Add AttendanceProgram
    - AttendanceProgram 클래스 추가

---
+++ AttendanceProgram/AttendaceProgram.swift
@@ -0,0 +1,11 @@
+//
+//  AttendaceProgram.swift
+//  AttendanceProgram
+//
+//  Created by 김민택 on 2023/03/16.
+//
+
+import Foundation
+
+class AttendanceProgram {
+}
---
+++ AttendanceProgram/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  AttendanceProgram
+//
+//  Created by 김민택 on 2023/03/16.
+//
+
+import Foundation
+
+let main = AttendanceProgram()
