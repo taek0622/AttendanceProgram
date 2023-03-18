fb2c8f6ac6f42fa9b5ba8c60801edfba5e3541bc
taek0622 <min4644@gmail.com>
2023-03-19 05:08:26 +0900

    Feat:sparkles:: Add TextLiteral
    - 텍스트 정보들을 저장하기 위한 텍스트 리터럴 추가

---
+++ AttendanceProgram/TextLiteral.swift
@@ -0,0 +1,14 @@
+//
+//  TextLiteral.swift
+//  AttendanceProgram
+//
+//  Created by 김민택 on 2023/03/17.
+//
+
+import Foundation
+
+enum TextLiteral {
+
+    static let programTitle = "# 스터디 출석부"
+    static let inputMark = ">"
+}
