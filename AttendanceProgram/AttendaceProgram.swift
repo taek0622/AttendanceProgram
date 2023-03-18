7c599cbfa278df4a6bdee59229f01091b6de0250
taek0622 <min4644@gmail.com>
2023-03-19 05:11:30 +0900

    Feat:sparkles:: Add default menu
    - 기본 메뉴 추가

--- AttendanceProgram/AttendaceProgram.swift
+++ AttendanceProgram/AttendaceProgram.swift
@@ -17,6 +17,16 @@ class AttendanceProgram {
 
     private func run() {
         while true {
+            print(TextLiteral.programTitle)
+            print("# 원하는 메뉴의 번호를 입력해주세요.")
+            print("1. 출석하기   2. 출석현황 조회   3. 이름 수정   4. 출석 삭제   5. 종료")
+            let input = receiveInput()
+
+            if Int(input!) == nil {
+                wrongInput()
+                continue
+            }
+
         }
     }
 }
